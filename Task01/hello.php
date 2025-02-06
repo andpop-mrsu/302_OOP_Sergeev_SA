4c61ddd92c352455957bcfa82d311f8073917b89
Kulebyaka1337 <sergeevserg032@gmail.com>
2025-02-06 20:07:58 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,8 @@
+<?php
+    date_default_timezone_set('Europe/Moscow');
+
+    $CurrentDateTime = date('Y-m-d H:i:s');
+    $FullNameAndLastname = 'Сергеев Сергей';
+
+    echo "Текущая дата и время: $CurrentDateTime\n";
+    echo "Фамилия и имя: $FullNameAndLastname\n";
